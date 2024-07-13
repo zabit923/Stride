68339e9dc403637bcee755c241f129a746fecdeb
IbragimovRr <92021334+IbragimovRr@users.noreply.github.com>
2024-07-14 00:55:03 +0300

    moduleCourse

--- Courses/Views/PageLayout.swift
+++ Courses/Views/PageLayout.swift
@@ -39,3 +39,35 @@ class PageLayout: UICollectionViewFlowLayout {
     }
     
 }
+
+class PageModuleLayout: UICollectionViewFlowLayout {
+    
+    var pageWidth: CGFloat = 345
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+
+        guard let collectionView = self.collectionView else {
+            let latestOffset = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+            return latestOffset
+        }
+
+        
+        // Make an estimation of the current page position.
+        let approximatePage = collectionView.contentOffset.x/pageWidth
+
+        // Determine the current page based on velocity.
+        let currentPage = velocity.x == 0 ? round(approximatePage) : (velocity.x < 0.0 ? floor(approximatePage) : ceil(approximatePage))
+
+        // Create custom flickVelocity.
+        let flickVelocity = velocity.x * 0.3
+
+        // Check how many pages the user flicked, if <= 1 then flickedPages should return 0.
+        let flickedPages = (abs(round(flickVelocity)) <= 1) ? 0 : round(flickVelocity)
+
+        // Calculate newHorizontalOffset.
+        let newHorizontalOffset = ((currentPage + flickedPages) * pageWidth)
+
+        return CGPoint(x: newHorizontalOffset, y: proposedContentOffset.y)
+    }
+    
+}
