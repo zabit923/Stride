802349986b3236004b608ae2fd000f1515964d15
IbragimovRr <92021334+IbragimovRr@users.noreply.github.com>
2024-07-23 18:16:43 +0300

    start 23

--- Courses/Controllers/OtherScreens/AddInfoAboutCourseVC.swift
+++ Courses/Controllers/OtherScreens/AddInfoAboutCourseVC.swift
@@ -20,20 +20,55 @@ class AddInfoAboutCourseVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        name.delegate = self
+        price.delegate = self
+        descriptionCourse.delegate = self
+        addCoach()
+    }
+    
+    func addCoach() {
+        let coach = UD().getMyInfo()
+        coachPred.text = "\(coach.name) \(coach.surname)"
     }
     
     @IBAction func save(_ sender: UIButton) {
+        
     }
     
     @IBAction func addImage(_ sender: UIButton) {
-        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true)
     }
     
     @IBAction func tap(_ sender: UITapGestureRecognizer) {
         price.resignFirstResponder()
         descriptionCourse.resignFirstResponder()
         name.resignFirstResponder()
+    }
+    
+}
+extension AddInfoAboutCourseVC: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imagePred.image = image
+            dismiss(animated: true)
+        }
+    }
+    
+}
+extension AddInfoAboutCourseVC: UITextFieldDelegate {
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        if textField == name {
+            namePred.text = textField.text
+        }else if textField == price {
+            pricePred.text = "\(textField.text!)$"
+        }
     }
 }
